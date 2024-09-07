bf4c766f0ba80680d5e29845af77f3b5f3bdae02
Sushant Raj <rsushant2000@gmail.com>
2024-09-07 23:48:04 +0530

    started lecture 2

---
+++ LectureTwo.py
@@ -0,0 +1,22 @@
+"""
+String: 
+string is data type that stores a sequence of characters.
+"hello"+"world"
+"""
+str1 = "this is a string"
+str2 = 'ApnaCollege'
+str3 = """This is also a string"""
+
+"""
+why three string, because some times we need to print the aposthropy
+in our code or string
+"""
+
+str1 = "This is a string.\nwe are creating it in python"
+print(str1)
+
+#Concatenation
+"hello"+"world" = "helloworld"
+
+
+
