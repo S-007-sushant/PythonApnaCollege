705bfb7d53bd4a695f41017b297c287b43150c87
Sushant Raj <rsushant2000@gmail.com>
2024-09-08 18:23:05 +0530

    lecture 2 complete

--- LectureTwo.py
+++ LectureTwo.py
@@ -59,4 +59,83 @@ print(str.count("am")) # counts the occurrence of substr in the string
 str = "This is the $ string which counts the $ in this statement $"
 print("this is the count of $",str.count("$"))
 
-#Conditional Statement at 30:38
+#Conditional Statement at 
+#1. if-elif-else (SYNTAX)
+
+"""
+if(condition):
+    statement1
+elif(condition):
+    statement2
+else:
+    statement
+"""
+"""age = 17
+if(age>=18):
+    print("can vote")
+else:
+    print("can't vote")
+
+light = "green"
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("lood")
+else:print("go")
+"""
+# marks = int(input("Enter marks in math: "))13
+# technique 1
+# if(marks>=90):
+#     grade = "A"
+# elif(marks>=80 and marks<90):
+#     grade = "B"
+# elif(marks>=70 and marks<80):
+#     grade = "C"
+# else : grade = "F"
+
+#technique 2
+"""if(marks<70):
+    grade = "F"
+elif(marks<80):
+    grade = "C"
+elif(marks<90):
+    grade = "B"
+else : grade = "A"
+
+print("Student marks is : ",grade)"""
+
+#Nesting: if under if
+age = 34
+if(age>=18):
+    if(age>=80):
+        print("Can't drive")
+    print("can drrive")
+else:
+    print("connot drive")
+
+
+"""
+practice time
+
+"""
+#1. write a program to check if a nunber entered by the user is odd or even
+# number = int(input("Enter a number: "))
+# if(number%2==0):
+#     print("Even Number")
+# else: print("Odd")
+
+#2. write a program to find the greatest of 3 numbers entered by the users
+# a = int(input("Enter first number : "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# if(a>b and a>c):
+#     print("a is greater number",a)
+# elif(b>a and b>c):
+#     print("b is greater number",b)
+# else: print("c is greater number",c)
+
+#3. write a program to checkk if a number is multiple of 7 or not
+number = int(input("Enter a number : "))
+if(number%7==0):
+    print("The number is divisible by 7: ",number)
+else: print("The number is not divisible by 7: ",number)
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-str = "This is the $ string which counts the $ in this statement $"
-print(str.find("$"))
