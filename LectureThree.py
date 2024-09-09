c258c5b42a85bfdab5d9d1e3ef657c6b8f686901
Sushant Raj <rsushant2000@gmail.com>
2024-09-10 01:36:23 +0530

    completed Lecture 3

--- LectureThree.py
+++ LectureThree.py
@@ -25,7 +25,7 @@ student = ["karan", 85, 17, "Delhi"]
 
 marks = [94, 95, 66, 45]
 print(marks[:-3])
-
+"""
 #List methods
 list = [2,1,3]
 print("acctual list",list)
@@ -39,6 +39,68 @@ list.reverse() #reverse list
 print("list reverse :",list)
 list.insert(2,5) #list.insert(index, element) insert element at index
 print("list insert at index 2 ",list)
+list.remove(1) # removes first occurrence of element
+print(list)
+list.pop() #list.pop(idc)removes element at idx
+print(list)
+list.pop(1)
+print(list)
+"""
+#Tuples in Python
+# a built in data type that lets us create immutable sequence of value
+"""tup = (87,64,33,44,55)
+print(tup,type(tup), tup[2])
+tupp = (1)
+tupp = (1,2,3,3,2)
+print(tup[1:3])
+print(tupp.index(1)) #return index of the first occurrence
+print(tupp.count(2))"""
+
+#practice
+#1. write a program to ask the user to enter name of their 3 favorite
+# movies and store them in a list
+
+"""movieList = []
+movieName1 = input("Enter a first Movie")
+movieList.append(movieName1)
+movieName2 = input("Enter second movie")
+movieList.append(movieName2)
+movieName3 = input("Enter third movie")
+movieList.append(movieName3)
+movies = []
+movies.append(input("Enter 1st movie"))
+movies.append(input("Enter 2st movie"))
+movies.append(input("Enter 3st movie"))
+
+print(movieList)"""
+
+# 2. write a program to check if a list contains a palindrome of elements 
+
+"""listName = [1,2,1]
+copyListName = listName.copy()
+copyListName.reverse()
+if(listName == copyListName):
+    print("Its a Parlinderom")
+else: print("Not Palindrome")"""
+
+#3. write a program to count the number of student with the "A" 
+#  grade in the following tuple
+gradTuple = ["C","D","A","A","B","B","A"]
+print(gradTuple.count("A"))
+
+#4. Store the above values in a list and sort them from "A to D"
+gradList = []
+for i in gradTuple:
+    gradList.append(i)
+print(gradList)
+gradList.sort(reverse=True)
+print("sorted :", gradList)
+
+ 
+
+
+
+
 
 
  
