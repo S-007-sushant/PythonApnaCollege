6db2ef42565b80bd1eedb8e44fe540b5c716bc78
Sushant Raj <rsushant2000@gmail.com>
2024-09-11 23:21:35 +0530

    lecture four

---
+++ LectureFour.py
@@ -0,0 +1,20 @@
+# Lecture Four : Dictionary and set in python
+""""
+Dictionnaries are used to store data value in key:value pairs
+they are unordered, mutable (changeable) and don't allow duplicate keys
+ex : 
+dict = {
+    "name": "Sushant"
+    "cgpa": 9.6
+    "marks" : [98,97,94],
+}
+"""
+info = {
+    "key":"value",
+    "name":"sushant raj",
+    "learning":"coding",
+    "age": 53,
+    "is_adult":True,
+    "marks":84
+}
+print(info)
