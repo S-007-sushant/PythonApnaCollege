92e7e54fca26571ee7c7871daf7a478e13caa85c
Sushant Raj <rsushant2000@gmail.com>
2024-09-23 23:21:39 +0530

    Completed LectureSeven

--- LectureSeven.py
+++ LectureSeven.py
@@ -42,5 +42,103 @@ f = open("demo.txt","a")
 f.write("this is a new line") adds to the file 
 """
 
-f = open("demo.txt","w")
-f.write("I wnat to learn Javascript")
+# f = open("demo.txt","w")
+# f.write("I wnat to learn Javascript")
+# f = open("demo.txt","r")
+# data = f.read()
+# print(data)
+# f.close
+
+"""appending the data"""
+
+# with syntax
+"""
+with open("demo.txt", "a") as f:
+    data = f.read()
+"""
+
+# with open("demo.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+# with open("demo.txt", "w") as f:
+#     f.write("Iron Man is Toney Stark")
+#     print(data)
+
+# deleting a file
+"""
+using the os module 
+Module (like a code library) is a file written by another programmer that
+generally has a functions we can use.
+"""
+# import os 
+
+# with open("sample.txt", "w") as f:
+#     f.write("Iron Man is Toney Stark")
+# os.remove("sample.txt") 
+
+# Practice time
+#1. Create a new file "practice.txt" using python. Add the following data in it:
+# Hi everyone
+# we are learning File I/O
+# using Java
+# I like programming in Java
+
+# with open("practice.txt","w") as f:
+#     f.write("Hi everyone\nwe are learning File I/O\nusing Java\nI like programming in Java")
+
+# with open("practice.txt","r") as f:
+#     print(f.read())
+
+#2. Write a function that replace all occurrence of "java" with "python" in above file
+# with open("practice.txt","r") as f:
+#     data = f.read()
+
+# new_data = data.replace("Java","Python")
+# print(new_data)
+
+# with open("practice.txt","w") as f:
+#     f.write(new_data)
+
+#3. Search if the word "learning" exists in the file or not
+
+# def checkWord(word):
+#     with open("practice.txt", "r") as f:
+#         data = f.read()
+#         if(data.find(word)!= -1):
+#             print("found")
+#         else : print("not found")
+
+# checkWord("sushant")
+
+#4. write a function in whihc line of the file does the word "learning"
+# occur first. Print -1 if word not found
+# def checkWordline(word):
+#     data = True
+#     line_no = 1
+#     with open("practice.txt", "r") as f:
+#         while data:
+#             data = f.readline()
+#             if(word in data):
+#                 print(line_no)
+#                 return
+#             line_no +=1
+#     return -1
+
+# result = checkWordline("pyq")
+# print(result)
+
+#4. From a file containg number separated by comma, print the count of even numbers
+# 1, 2, 76, 84, 90, 101
+count = 0
+with open("practice.txt","r") as f:
+    data = f.read()
+    print(data)
+    
+    num = data.split(",")
+    print(num)
+    for i in num:
+        if(int(i) % 2 == 0):
+            count+=1
+        
+print(count)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-
-# printlinebyline(cities)
-
-#3. Write a function to find the factorial of n (n is the p
+ 
