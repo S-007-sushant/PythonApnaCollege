42cc7dc8db249b5ae4c7a06cc19aebb4362e3d74
Sushant Raj <rsushant2000@gmail.com>
2024-10-05 23:44:46 +0530

    Lecture Eight

---
+++ LectureEight.py
@@ -0,0 +1,30 @@
+# Lecture Eight : OOP's
+
+"""
+To map with real world scenarios, we started suing objects in code.
+This is called Object Oriendted programming
+
+procedural -> functional -> oops
+
+Class - Class is a blueprint for creating objects
+Objects - The objects is an entity that has a state and behavior associated with it
+    
+"""
+# ["sushant", 98,]  
+# class Student:
+#     name="Sushant"
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s1.name)
+
+class Car:
+    color = "blue"
+    brand = "mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand )
+     
